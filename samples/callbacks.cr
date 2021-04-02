0b571e2aa3ddb484d99ec68153dafa7bc6b056f9
Anton Maminov <anton.linux@gmail.com>
2021-04-02 17:29:35 +0300

    update callbacks sample

--- samples/callbacks.cr
+++ samples/callbacks.cr
@@ -10,6 +10,10 @@ client.on :song do
   if current_song = client.currentsong
     puts "[#{Time.local}] 🎵 #{current_song["Artist"]} - #{current_song["Title"]}"
   end
+
+  if next_song = client.nextsong
+    puts "[#{Time.local}] 🔜 #{next_song["Artist"]} - #{next_song["Title"]}"
+  end
 end
 
 loop do
