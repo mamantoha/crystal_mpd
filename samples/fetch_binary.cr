bff1772d2a1203f6d45fc17dc3d90d0305a4e4fd
Anton Maminov <anton.linux@gmail.com>
2022-01-16 14:05:32 +0200

    Create fetch_binary.cr

---
+++ samples/fetch_binary.cr
@@ -0,0 +1,22 @@
+require "mime"
+require "../src/crystal_mpd"
+
+# MPD::Log.level = :debug
+# MPD::Log.backend = ::Log::IOBackend.new
+
+client = MPD::Client.new
+
+if (song = client.currentsong)
+  if (response = client.readpicture(song["file"]))
+    data, binary = response
+
+    p! data
+
+    extension = MIME.extensions(data["type"]).first? || ".png"
+
+    file = File.new("cover#{extension}", "w")
+    file.write(binary.to_slice)
+  end
+end
+
+client.disconnect
