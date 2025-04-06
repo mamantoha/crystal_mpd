d1945fcd6d7278811cce769afd025010f3638219
Anton Maminov <anton.linux@gmail.com>
2025-04-06 21:21:55 +0300

    introduce MockMPDServer

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,67 +1,14 @@
 require "spec"
 require "../src/crystal_mpd"
-
-def handle_client(client : TCPSocket)
-  client.puts("OK MPD 0.24.2")
-
-  while line = client.gets
-    case line
-    when .starts_with?("find ")
-      expression = line.split(" ", 2)[1]
-
-      if expression == "\"(Artist == \\\"Nirvana\\\")\" "
-        client.puts("file: music/foo.mp3")
-        client.puts("Title: Smells Like Teen Spirit")
-        client.puts("Artist: Nirvana")
-        client.puts("OK")
-      else
-        client.puts("OK")
-      end
-    when .starts_with?("playid ")
-      song_id = line.split(" ")[1].to_i
-
-      if song_id == 1
-        client.puts("OK")
-      else
-        client.puts("ACK [50@0] {playid} No such song")
-      end
-    when .starts_with?("status")
-      client.puts("volume: 100")
-      client.puts("repeat: 0")
-      client.puts("random: 0")
-      client.puts("single: 0")
-      client.puts("consume: 0")
-      client.puts("partition: default")
-      client.puts("playlist: 88")
-      client.puts("playlistlength: 27")
-      client.puts("mixrampdb: 0")
-      client.puts("lastloadedplaylist: ")
-      client.puts("song: 7")
-      client.puts("songid: 37")
-      client.puts("time: 15:237")
-      client.puts("elapsed: 14.690")
-      client.puts("bitrate: 128")
-      client.puts("duration: 237.253")
-      client.puts("audio: 44100:f:2")
-      client.puts("nextsong: 24")
-      client.puts("nextsongid: 54")
-      client.puts("OK")
-    else
-      # for debug purposes
-      client.puts("ACK [5@0] {} unknown command `#{line}`")
-    end
-  end
-ensure
-  client.close
-end
+require "./support/mock_mpd_server"
 
 def with_server(host = "localhost", port = 6600, &)
   wants_close = Channel(Nil).new
-  server = TCPServer.new(host, port)
+  server = MockMPDServer.new(host, port)
 
   spawn do
     while client = server.accept?
-      spawn handle_client(client)
+      spawn server.handle_client(client)
     end
   end
 
